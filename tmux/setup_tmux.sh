f1de1472227e160a641a6a0a0cc1aa2277dcfac6
William Lenoir <WilliamLenoir@users.noreply.github.com>
2024-03-03 23:17:59 +0100

    refacto: move tmux folder

--- tmux/setup_tmux.sh
+++ tmux/setup_tmux.sh
@@ -9,7 +9,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # create a symbolic link on the versioned tmux config
 # which is in th neovim repo
 # TODO: move this config in a dedicated repo or in a global .config repo
-ln -s ~/.config/nvim/tmux/.tmux.conf ~/.tmux.conf
+ln -s .tmux.conf ~/.tmux.conf
 
 echo "Now you can run tmux"
 echo "> tmux"
