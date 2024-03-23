f256740f9a1d8aa80d61945ce3e08749c6153da5
William Lenoir <WilliamLenoir@users.noreply.github.com>
2024-03-23 22:27:17 +0100

    feat(quickfixlist): no preview, remove transparency and merge two files

--- nvim/lua/plugins/quickfix.lua
+++
@@ -1,8 +0,0 @@
-return {
-  -- improve the quickfix list UI
-  -- NOTE: do not install the reflector plugin, both are exclusive
-  {
-    "kevinhwang91/nvim-bqf",
-    dependencies = { "junegunn/fzf" },
-  },
-}
--- nvim/lua/plugins/quickfixlist.lua
+++ nvim/lua/plugins/quickfixlist.lua
@@ -3,4 +3,16 @@ return {
   {
     "itchyny/vim-qfedit",
   },
+  -- improve the quickfix list UI
+  -- NOTE: do not install the reflector plugin, both are exclusive
+  {
+    "kevinhwang91/nvim-bqf",
+    dependencies = { "junegunn/fzf" },
+    opts = {
+      preview = {
+        auto_preview = false,
+        winblend = 0,
+      },
+    },
+  },
 }
