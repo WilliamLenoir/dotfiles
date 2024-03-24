500a41702663312cf0f28b49fbec75a94c82f269
William Lenoir <WilliamLenoir@users.noreply.github.com>
2024-03-24 17:12:44 +0100

    feat(search): use / for fuzzy search and g/ for standard search

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,6 +1,22 @@
 return {
   -- we use fzf for the bqf plugin (for the quickfix list)
   {
-    "junegunn/fzf"
-  }
+    -- important: use the full fqdn (otherwise the plugin won't be loaded)
+    "https://github.com/junegunn/fzf.vim",
+    lazy = false,
+  },
+  {
+    -- fuzzy search with /
+    "ggvgc/vim-fuzzysearch",
+    lazy = false,
+    config = function()
+      vim.cmd([[
+        " use / to launch a fuzzy search (replace the default methode)
+        nnoremap / :FuzzySearch<CR>
+        " use g/ to use the standard search
+        nnoremap g/ /
+        let g:fuzzysearch_prompt = '/'
+      ]])
+    end,
+  },
 }
