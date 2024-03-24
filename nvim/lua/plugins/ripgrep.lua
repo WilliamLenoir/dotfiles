cd04102f168a363b50591290a1c3e39df26f0fc2
William Lenoir <WilliamLenoir@users.noreply.github.com>
2024-03-24 17:13:20 +0100

    feat(search): add example query and quotes with ripgrep command

--- nvim/lua/plugins/ripgrep.lua
+++ nvim/lua/plugins/ripgrep.lua
@@ -3,7 +3,13 @@ return {
     "jremmen/vim-ripgrep",
     cmd = "Rg",
     keys = {
-      { mode = { "n" }, "<leader>rg", "<esc>:Rg ",        desc = "Search with RipGrep",     { noremap = true } },
+      {
+        mode = { "n" },
+        "<leader>rg",
+        "<esc>:Rg '' -g '**/*'<left><left><left><left><left><left><left><left><left><left><left>",
+        desc = "Search with RipGrep",
+        { noremap = true },
+      },
       { mode = { "v" }, "<leader>rz", 'y<esc>:Rg <c-r>"', desc = "Search THIS with RipGrep" },
     },
   },
