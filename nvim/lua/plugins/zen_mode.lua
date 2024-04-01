1554571a4cf242cc621c96c200a6f959d05babce
William Lenoir <WilliamLenoir@users.noreply.github.com>
2024-04-02 00:08:34 +0200

    feat(zen_mode): change keymaps

--- nvim/lua/plugins/zen_mode.lua
+++ nvim/lua/plugins/zen_mode.lua
@@ -2,7 +2,7 @@ return {
   {
     "folke/zen-mode.nvim",
     keys = {
-      { "<leader>fo", "<cmd>ZenMode<cr>", desc = "🧘 Toogle ZenMode 🧘" },
+      { "<leader>zm", "<cmd>ZenMode<cr>", desc = "🧘 Toogle ZenMode 🧘" },
     },
     cmd = "ZenMode",
     opts = {
@@ -13,17 +13,17 @@ return {
         -- * a percentage of the width / height of the editor when <= 1
         -- * a function that returns the width or the height
         width = 120, -- width of the Zen window
-        height = 1,  -- height of the Zen window
+        height = 1, -- height of the Zen window
         -- by default, no options are changed for the Zen window
         -- uncomment any of the options below, or add other vim.wo options you want to apply
         options = {
-          signcolumn = "no",      -- disable signcolumn
-          number = false,         -- disable number column
+          signcolumn = "no", -- disable signcolumn
+          number = false, -- disable number column
           relativenumber = false, -- disable relative numbers
-          cursorline = false,     -- disable cursorline
-          cursorcolumn = false,   -- disable cursor column
-          foldcolumn = "0",       -- disable fold column
-          list = false,           -- disable whitespace characters
+          cursorline = false, -- disable cursorline
+          cursorcolumn = false, -- disable cursor column
+          foldcolumn = "0", -- disable fold column
+          list = false, -- disable whitespace characters
         },
       },
       plugins = {
@@ -31,15 +31,15 @@ return {
         -- comment the lines to not apply the options
         options = {
           enabled = true,
-          ruler = false,   -- disables the ruler text in the cmd line area
+          ruler = false, -- disables the ruler text in the cmd line area
           showcmd = false, -- disables the command in the last line of the screen
           -- you may turn on/off statusline in zen mode by setting 'laststatus'
           -- statusline will be shown only if 'laststatus' == 3
-          laststatus = 0,               -- turn off the statusline in zen mode
+          laststatus = 0, -- turn off the statusline in zen mode
         },
-        twilight = { enabled = true },  -- enable to start Twilight when zen mode opens
+        twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
         gitsigns = { enabled = false }, -- disables git signs
-        tmux = { enabled = false },     -- disables the tmux statusline
+        tmux = { enabled = false }, -- disables the tmux statusline
         -- this will change the font size on kitty when in zen mode
         -- to make this work, you need to set the following kitty options:
         -- - allow_remote_control socket-only
@@ -64,11 +64,9 @@ return {
         },
       },
       -- callback where you can add custom code when the Zen window opens
-      on_open = function(win)
-      end,
+      on_open = function(win) end,
       -- callback where you can add custom code when the Zen window closes
-      on_close = function()
-      end,
-    }
-  }
+      on_close = function() end,
+    },
+  },
 }
